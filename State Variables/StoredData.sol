792b52c0b3a3749e107a91ba23caea5b01b63d90
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-16 07:51:49 +0500

    Create StoredData.sol

---
+++ State Variables/StoredData.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract StoringData_in_BlockChain {
+
+           uint stateVariable ; // stored data
+
+           constructor()  {
+                stateVariable = 20; 
+           }
+}
+
