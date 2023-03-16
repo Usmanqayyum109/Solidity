4b99681fe9f0ead66ac12d8a2089ff71599c067c
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-16 09:26:13 +0500

    Create Pure.sol

---
+++ Functions/Pure.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Specifier {
+
+           function getValue() public pure returns(uint){
+                uint num = 100;
+                return num;
+           }
+}
