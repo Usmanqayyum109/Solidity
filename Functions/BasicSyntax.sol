9fe9d4da3d67ea42b47227665c23ab6f10e7abd7
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-16 08:24:27 +0500

    Create BasicSyntax.sol

---
+++ Functions/BasicSyntax.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract BasicSyntax {
+
+           function setValue() public {
+                
+                // statement
+           }
+
+           function setValueWithParameter() public {
+
+                // statement
+           }
+
+           function getValue() public returns(uint) {
+                // retrun statement
+           }
+
+           function getValueWithParameters (uint _a) public returns(uint) {
+                // return statement
+           }
+
+}
+
