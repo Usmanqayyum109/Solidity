f34b9c3ba42a3af1efbe78a5791c46b7152864c7
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-16 09:28:16 +0500

    Create View.sol

---
+++ Functions/View.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Specifier {
+
+          uint num = 50;
+
+           function getValue() public view returns(uint){
+               
+                return num;
+           }
+}
