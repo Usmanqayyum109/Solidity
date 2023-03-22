d68cdc3fe8bfe5effc2caa4dc1398d9537b1edef
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-22 11:02:10 +0500

    Create Variable_declare_in_function.sol

---
+++ Data Location/Variable_declare_in_function.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Variable_declare_in_functioin {
+
+    function getValue() external pure  {
+
+        uint num = 4;   // store in memory     
+    }
+    }
