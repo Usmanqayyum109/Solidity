8fda21ea9d01e4e9470abbeb84bccdd34ea415a2
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-23 09:48:03 +0500

    Create Fixed.sol

---
+++ Array/Fixed.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract FixedArray{
+
+    int[2] num = [int(4), 6];
+
+    string[4] week = [string("monday"), "tuesday", "wednesday", "thursday"];
+
+    }
