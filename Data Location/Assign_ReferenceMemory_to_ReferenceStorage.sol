a3b78e7bf16ab2933df5e4e52682fbf447bcb321
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-23 09:40:18 +0500

    Create Assign_ReferenceMemory_to_ReferenceStorage.sol

---
+++ Data Location/Assign_ReferenceMemory_to_ReferenceStorage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Assign_ReferenceMemory_to_ReferenceStorage{
+
+    int[2] num = [int(4), 6];
+
+    function assignment8() public  returns (int) {
+
+        int[2] storage mynum = num;
+        mynum[1] = 10;
+
+        return num[1];
+    }
+
+    }
