3f036f8b6813669df4ad4109ac832b5ddd44f8e8
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-23 09:51:46 +0500

    Create Dynamic.sol

---
+++ Array/Dynamic.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract DynamicArray{
+
+    int[2] num = [int(4), 6];
+
+    string[] week = new string[] (6);
+
+    }
