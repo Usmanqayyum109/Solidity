dcd5d1af80c0af0270b302afea3fa5d547b6e74b
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-25 10:02:47 +0500

    Create LogicalNOT.sol

---
+++ Logical Operators/LogicalNOT.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract LogicalNOT{
+
+        bool public value = !(10 == 5);
+        bool public value2 = !(10 == 10);
+        
+}
