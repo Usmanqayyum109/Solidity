2344a78270a2891792a7e7fa75a4e5fc8d0a1fe3
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-25 09:51:20 +0500

    Create GreaterThanOrEqual.sol

---
+++ Comparison Operators/GreaterThanOrEqual.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract GreaterThanOrEqual{
+
+        bool public value = 10 >= 40;
+        bool public value2 = 6 >= 6;
+        
+}
