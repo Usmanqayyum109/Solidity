51b816bebdaf7b2345c01dea1fbdfee61aa18408
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-25 09:57:29 +0500

    Create LogicalOR.sol

---
+++ Logical Operators/LogicalOR.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract LogicalOR{
+
+        bool public value = 10 > 9 || 5 < 7;
+        bool public value2 = 6 > 6 || 8 < 9;
+        
+}
