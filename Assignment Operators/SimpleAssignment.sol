6a84c4f4b9245a0a8eadac22a0b87f4543cb158d
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-25 10:08:42 +0500

    Create SimpleAssignment.sol

---
+++ Assignment Operators/SimpleAssignment.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract SimpleAssigment{
+
+        uint value1 = 5;
+        uint value2 = 9;
+        uint public sum = value1 + value2;
+        
+}
