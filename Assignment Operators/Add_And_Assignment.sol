3ee58f22c1a27831a564f53db0daeba409cd54cc
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-25 10:16:09 +0500

    Create Add_And_Assignment.sol

---
+++ Assignment Operators/Add_And_Assignment.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Add_And_Assignment{
+
+        uint public value1 = 15;
+        
+        constructor() {
+
+             value1 += 5;
+        }
+}
