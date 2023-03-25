bb4d2db503e2e4e5a51459cf50cfb37f50f66bac
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2023-03-25 09:46:52 +0500

    Create GreaterThan.sol

---
+++ Comparison Operators/GreaterThan.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT -3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract GreaterThan{
+
+        bool public value = 10 > 4;
+        bool public value2 = 6 > 10;
+        
+}
